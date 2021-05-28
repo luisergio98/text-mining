98bd1372e3a3b6194f0c5d3a4491ab4303b627a7
Luis Joppert <luisergio98@gmail.com>
2021-05-28 11:01:51 -0300

    PCA fix and manual processing on example texts

--- vectorial_pca.py
+++ vectorial_pca.py
@@ -1,11 +1,12 @@
-import nltk
-import pandas as pd
-import numpy as np
-import regex as re
 import collections
 import random
-from matplotlib import pyplot as plt
+
+import nltk
+import numpy as np
+import pandas as pd
+import regex as re
 from gensim.models import Word2Vec
+from matplotlib import pyplot as plt
 from nltk.corpus import stopwords
 
 nltk.download('punkt')
@@ -40,6 +41,7 @@ def get_most_frequent_words(text, quantity):
 def show_similarities(model, top_words):
     count = len(top_words)
     word_array = []
+    print("\n")
     while count > 0:
         word_1 = word_2 = ' '
         while len([word for word in word_array if word_1 in word and word_2 in word]) > 0 or word_1 == word_2:
@@ -50,15 +52,16 @@ def show_similarities(model, top_words):
         print(model.wv.similarity(word_1, word_2) * 100, end="%\n")
         word_array.append((word_1, word_2))
         count -= 1
+    print("\n")
 
 
-def plot_pca(model, content=None, top=False):
-    if top:
-        x = [model.wv[word] for word in model.wv.key_to_index if len([top for top in content if word in top]) > 0]
-        all_words = [word[0] for word in content]
-    else:
-        x = model.wv[model.wv.key_to_index]
-        all_words = list(model.wv.key_to_index)
+def plot_pca(model, annotate=True):
+    x = model.wv[model.wv.key_to_index]
+    all_words = list(model.wv.key_to_index)
+
+    if annotate:
+        print("\n")
+        print(x, end="\n")
 
     x_corr = pd.DataFrame(x).corr()
     values, vectors = np.linalg.eig(x_corr)
@@ -66,32 +69,38 @@ def plot_pca(model, content=None, top=False):
     args = (-values).argsort()
     vectors = vectors[:, args]
     new_vectors = vectors[:, :2]
-
     new_x = np.dot(x, new_vectors)
 
     plt.figure(figsize=(13, 7))
-    plt.scatter(new_x[:, 0], new_x[:, 1], linewidths=10, color='green')
+    plt.scatter(np.real(new_x[:, 0]), np.real(new_x[:, 1]), linewidths=10, color='green')
     plt.xlabel("PC1", size=15)
     plt.ylabel("PC2", size=15)
-    plt.title("Word Embedding Space", size=20)
-    for i, word in enumerate(all_words):
-        plt.annotate(word, xy=(new_x[i, 0], new_x[i, 1]))
+    plt.title("PCA", size=20)
+    if annotate:
+        for i, word in enumerate(all_words):
+            plt.annotate(word, xy=(np.real(new_x[i, 0]), np.real(new_x[i, 1])))
 
     plt.show()
 
 
 def run():
     text = prepare_text(open("Texts/OFilhoEterno-CristovaoTezza.txt", "r", encoding="utf8").read())
-
     top_words = get_most_frequent_words(text, 10)
-
     tokens = text_to_tokens(text)
 
+    # Model made from the most frequent words
+    model = Word2Vec(tokens, vector_size=50, sg=1, min_count=min(top_words, key=lambda t: t[1])[1])
+    plot_pca(model)
+
+    # Reducing the word array using the minimum count of 30 to improve the plot readability
     model = Word2Vec(tokens, vector_size=50, sg=1, min_count=30)
+    plot_pca(model)
+
+    # Using the default minimum count (5) but with no word annotation to improve the plot readability
+    model = Word2Vec(tokens, vector_size=50, sg=1)
+    plot_pca(model, False)
 
     show_similarities(model, top_words)
-
-    plot_pca(model)
 
 
 if __name__ == '__main__':
